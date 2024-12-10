c6a4a38f7595c12e77d7685a46ce8ffe95bfd674
ChiriacGeorgeBogdan <bogdang.chiriac@gmail.com>
2024-12-11 00:20:15 +0200

    Add files via upload

--- Electron_01/main.cpp
+++ Electron_01/main.cpp
@@ -10,22 +10,23 @@ using namespace std;
 const int LATIME_ECRAN = 1600;
 const int INALTIME_ECRAN = 800;
 
-    ///Item barul este aflat langa marginea de sus a ecranului y=0
+///Item barul este aflat langa marginea de sus a ecranului y=0
 const int INALTIMEA_BAREI_DE_ITEME = 50;   /// inaltimea Item BAR
-    ///Barul de tooluri este situat langa marginea stanga a ecranului (x=0) si sub barul de iteme
+///Barul de tooluri este situat langa marginea stanga a ecranului (x=0) si sub barul de iteme
 const int LATIME_TOOLBAR = 150;
 const int NR_TOOLS=7;
 const int NR_ITEME=13;
 
 const int MAX_PIESE = 100;  /// Nr maxim de piese pe care le putem desena
 
-const char Tool_Labels[NR_TOOLS][20]={"Clear Mouse", "Rotate" ,"Move","Erase Shape", "Make Connection", "Erase All", "Erase Connection"};
-const char Item_Labels[NR_ITEME][15]={"Shape 1"};
+const char Tool_Labels[NR_TOOLS][20]= {"Make Connection", "Rotate","Move","Erase Shape", "Clear Mouse", "Erase All", "Erase Connection"};
+const char Item_Labels[NR_ITEME][15]= {"Shape 1"};
 
 
 //fisierele si structura aferenta figurilor
-const char fisiere[20][15]={"AMPLOP.PS","BATERIE.PS","CONDENS.PS","DIODA.PS","NOD.PS","POLARIZ.PS","REZIST.PS","SERVOMOT.PS","SINU.PS","STOP.PS","TRANZNPN.PS","TRANZPNP.PS","ZENNER.PS"};
-struct figura{
+const char fisiere[20][15]= {"AMPLOP.PS","BATERIE.PS","CONDENS.PS","DIODA.PS","NOD.PS","POLARIZ.PS","REZIST.PS","SERVOMOT.PS","SINU.PS","STOP.PS","TRANZNPN.PS","TRANZPNP.PS","ZENNER.PS"};
+struct figura
+{
     char nume[50];
     int nr_intrari;
     double intrari[5][2];
@@ -34,7 +35,7 @@ struct figura{
     double bucati[20][4];
     char tip_bucata[20];
     int marire;
-}figuri[50];
+} figuri[50];
 
 void citire_figura(int index)
 {
@@ -71,10 +72,24 @@ void citire_figuri()
 
 
 /// Memoreaza toate piesele aflate pe ecran
-struct piesa{
+
+struct intrare
+{
+    int x;
+    int y;
+};
+struct piesa
+{
     int x1,y1,x2,y2;//colt stanga sus si colt dreapta jos
     int x,y,index;
+    intrare intrari[5];
 } piese[MAX_PIESE];
+
+
+struct grafuri{
+    int intrari[5][5];
+}graf[MAX_PIESE][MAX_PIESE];
+
 int nrPiese = -1; /// Nr de piese aflate pe ecran
 
 /// Deseneaza Bara de Iteme/Piese
@@ -84,21 +99,46 @@ void DeseneazaBaraDeIteme()
 
     for (int i = 0; i < NR_ITEME; ++i)
     {
+
         setfillstyle(SOLID_FILL, LIGHTGRAY);
         bar(i * Lungimea_Barei_Iteme, 0, (i + 1) * Lungimea_Barei_Iteme, INALTIMEA_BAREI_DE_ITEME);
 
         setcolor(BLACK);
         rectangle(i * Lungimea_Barei_Iteme, 0, (i + 1) * Lungimea_Barei_Iteme, INALTIMEA_BAREI_DE_ITEME);
-    }
 
-    /// Numirea piselor/itemelor
-    setbkcolor(LIGHTGRAY);
-    settextstyle(DEFAULT_FONT, HORIZ_DIR, 1);
-    setcolor(BLACK);
-    for (int i = 0; i < NR_ITEME; ++i) {
-        outtextxy(i * (LATIME_ECRAN / NR_ITEME) + 10, 15, figuri[i].nume);
+        /// Desenam figurile in meniul de iteme
+
+        setcolor(COLOR(255, 255, 51));
+        ///Centram coordonatele
+        int x = i * Lungimea_Barei_Iteme + Lungimea_Barei_Iteme / 2;
+        int y = INALTIMEA_BAREI_DE_ITEME / 2;
+        int index = i;
+
+
+        for (int j = 0; j < figuri[index].nr_bucati; ++j)
+        {
+            char type = figuri[index].tip_bucata[j];
+            int a = figuri[index].marire * figuri[index].bucati[j][0];
+            int b = figuri[index].marire * figuri[index].bucati[j][1];
+            int c = figuri[index].marire * figuri[index].bucati[j][2];
+            int d = figuri[index].marire * figuri[index].bucati[j][3];
+
+            if (type == 'L')
+            {
+                line(x + a, y + b, x + c, y + d);
+            }
+            else if (type == 'O')
+            {
+                ellipse(x + a, y + b, 0, 360, c, d);
+            }
+            else if (type == 'R')
+            {
+                rectangle(x + a, y + b, x + c, y + d);
+            }
+        }
     }
 }
+
 /// Deseneaza Bara de Tooluri
 void DeseneazaBaraDeTools()
 {
@@ -171,7 +211,6 @@ bool sePoateDesena(piesa piesaNoua, int x, int y, int index)
 }
 void desenare_piesa (int x, int y, int index)
 {
-    setbkcolor(BLACK);
     setcolor(COLOR(255,255,51));
     for (int i=0; i<figuri[index].nr_bucati; ++i)
     {
@@ -180,15 +219,30 @@ void desenare_piesa (int x, int y, int index)
         int b=figuri[index].marire*figuri[index].bucati[i][1];
         int c=figuri[index].marire*figuri[index].bucati[i][2];
         int d=figuri[index].marire*figuri[index].bucati[i][3];
-        if (type=='L') {
+        if (type=='L')
+        {
             line(x+a,y+b,x+c,y+d);
         }
-        else if (type=='O') {
+        else if (type=='O')
+        {
             ellipse(x+a,y+b,0,360,c,d);
         }
-        else if (type=='R') {
+        else if (type=='R')
+        {
             rectangle(x+a,y+b,x+c,y+d);
         }
+    }
+}
+void calcul_intratri(piesa &piesaNoua)
+{
+    int index=piesaNoua.index;
+    int a, b;
+    for(int j=0; j<figuri[index].nr_intrari; ++j)
+    {
+        a=figuri[index].intrari[j][0]*figuri[index].marire;
+        b=figuri[index].intrari[j][1]*figuri[index].marire;
+        piesaNoua.intrari[j].x=a+piesaNoua.x;
+        piesaNoua.intrari[j].y=b+piesaNoua.y;
     }
 }
 void incadrare (piesa& piesaNoua, int x, int y, int index)
@@ -202,19 +256,26 @@ void incadrare (piesa& piesaNoua, int x, int y, int index)
         int b=figuri[index].marire*figuri[index].bucati[i][1];
         int c=figuri[index].marire*figuri[index].bucati[i][2];
         int d=figuri[index].marire*figuri[index].bucati[i][3];
-        if (type=='L') {
-            piesaNoua.x1=min(piesaNoua.x1,x+a);
+        if (type=='L')
+        {
+            /*piesaNoua.x1=min(piesaNoua.x1,x+a);
             piesaNoua.y1=min(piesaNoua.y1,y+b);
             piesaNoua.x2=max(piesaNoua.x2,x+c);
-            piesaNoua.y2=max(piesaNoua.y2,y+d);
+            piesaNoua.y2=max(piesaNoua.y2,y+d);*/
+            piesaNoua.x1 = min(min(piesaNoua.x1, x + a), x + c);
+            piesaNoua.y1 = min(min(piesaNoua.y1, y + b), y + d);
+            piesaNoua.x2 = max(max(piesaNoua.x2, x + a), x + c);
+            piesaNoua.y2 = max(max(piesaNoua.y2, y + b), y + d);
         }
-        else if (type=='O') {
+        else if (type=='O')
+        {
             piesaNoua.x1=min(piesaNoua.x1,x+a-c);
             piesaNoua.y1=min(piesaNoua.y1,y+b-d);
             piesaNoua.x2=max(piesaNoua.x2,x+a+c);
             piesaNoua.y2=max(piesaNoua.y2,y+b+d);
         }
-        else if (type=='R') {
+        else if (type=='R')
+        {
             piesaNoua.x1=min(piesaNoua.x1,x+a);
             piesaNoua.y1=min(piesaNoua.y1,y+b);
             piesaNoua.x2=max(piesaNoua.x2,x+c);
@@ -224,23 +285,53 @@ void incadrare (piesa& piesaNoua, int x, int y, int index)
     piesaNoua.x=x;
     piesaNoua.y=y;
     piesaNoua.index=index;
+    calcul_intratri(piesaNoua);
 }
 /// Functie care detecta ce Item a fost apasat
-int getItemIndex(int x, int y) {
+int getItemIndex(int x, int y)
+{
     if (y > INALTIMEA_BAREI_DE_ITEME) return -1;
     return x / (LATIME_ECRAN / NR_ITEME);
 }
+int getToolIndex(int x, int y)
+{
+    if(x>LATIME_TOOLBAR || y<=INALTIMEA_BAREI_DE_ITEME) return -1;
+    int TOOLS_Inaltime = (INALTIME_ECRAN - INALTIMEA_BAREI_DE_ITEME) / NR_TOOLS;
+    return (y - INALTIMEA_BAREI_DE_ITEME) / TOOLS_Inaltime;
+}
+void drawLine(int x1, int y1, int x2, int y2, int cul)
+{
+    setcolor(cul);
+    line(x1, y1, (x1 + x2) / 2, y1);
+    line((x1 + x2) / 2, y1, (x1 + x2) / 2, y2);
+    line((x1 + x2) / 2, y2, x2, y2);
+}
 
+void desenare_legaturi()
+{
+    for(int i=0; i<=nrPiese; ++i)
+    {
+        for(int j=0; j<=nrPiese; ++j)
+        {
+                for(int k=0; k<5; ++k)
+                {
+                    for(int u=0; u<5; ++u)
+                        if(graf[i][j].intrari[k][u]==1)
+                            drawLine(piese[i].intrari[k].x, piese[i].intrari[k].y, piese[j].intrari[u].x, piese[j].intrari[u].y , RED);
+                }
+        }
+    }
+}
 
 void redraw()
 {
-    setbkcolor(BLACK);
     DeseneazaBaraDeIteme();
     DeseneazaBaraDeTools();
     for (int i=0; i<=nrPiese; ++i)
         desenare_piesa(piese[i].x,piese[i].y,piese[i].index);
+    desenare_legaturi();
 }
-void animareChenar (int state)
+void AnimareChenar (int state)
 {
     setbkcolor(BLACK);
     setcolor(WHITE);
@@ -261,11 +352,172 @@ void animareChenar (int state)
     }
 }
 
-void stergePiesa (int index)
+const int Raza_Intrare=5;
+const int Imprecizie=25;
+
+void desenare_intrari()
 {
-    piese[index]=piese[nrPiese--];
+    setcolor(WHITE);
+    for (int i = 0; i <= nrPiese; ++i)
+        for (int j = 0; j < figuri[piese[i].index].nr_intrari; ++j)
+            ellipse(piese[i].intrari[j].x, piese[i].intrari[j].y, 0, 360, Raza_Intrare, Raza_Intrare);
 }
-int main() {
+
+
+bool mouse_se_intersecteaza_cu_piesa(int x, int y, piesa a)
+{
+    return (((a.x2+Imprecizie)>=x && x>=(a.x1-Imprecizie)) && ((a.y2+Imprecizie)>=y && y>=(a.y1-Imprecizie)));
+}
+int mouse_se_interserteaza_cu_intrarea_unei_piese(int x, int y, piesa a)
+{
+    for(int j = 0; j < figuri[a.index].nr_intrari; ++j)
+    {
+        int x1=a.intrari[j].x-Raza_Intrare-Imprecizie/2;
+        int x2=a.intrari[j].x+Raza_Intrare+Imprecizie/2;
+        int y1=a.intrari[j].y-Raza_Intrare-Imprecizie/2;
+        int y2=a.intrari[j].y+Raza_Intrare+Imprecizie/2;
+        if((x>=x1 && x<=x2) && (y>=y1 && y<=y2))
+            return j;
+    }
+    return -1;
+}
+int index_figura_apasata(int x, int y)
+{
+    if(getItemIndex(x, y)!=-1 || getToolIndex(x, y)!=-1)
+        return -1;
+    for(int i=0; i<=nrPiese; ++i)
+    {
+        if(mouse_se_intersecteaza_cu_piesa(x, y, piese[i]))
+            return i;
+    }
+    return -1;
+}
+
+void identificare_nod(bool &running, int &index, int &index_intrare, int x, int y)
+{
+    index = index_figura_apasata(x, y);
+    if (index == -1)
+    {
+        cout<<"Nu exista o piesa la ("<< x << ", " << y << ")"<<endl;
+        running = false;
+        return;
+    }
+
+    index_intrare = mouse_se_interserteaza_cu_intrarea_unei_piese(x, y, piese[index]);
+    if (index_intrare == -1)
+    {
+        cout<<"Nu exista o intratre pentru piesa "<<index<<" la: (" << x << ", " << y << ")"<<endl;
+        running = false;
+        return;
+    }
+
+    cout<<"Nod identificat: pentru Piesa "<<index<<",la intrarea cu indexul: "<<index_intrare<<endl;
+}
+
+
+void trasare_legatura()
+{
+    int Index_01 = -1, Index_intrare_01 = -1;
+    int Index_02 = -1, Index_intrare_02 = -1;
+    bool running_desenare_legaturi = true;
+
+    while (running_desenare_legaturi)
+    {
+        if (ismouseclick(WM_LBUTTONDOWN))
+        {
+            int x = mousex();
+            int y = mousey();
+            clearmouseclick(WM_LBUTTONDOWN);
+
+            cout<<"Primul click la: (" << x << ", " << y << ")"<<endl;
+
+            identificare_nod(running_desenare_legaturi, Index_01, Index_intrare_01, x, y);
+
+            if (Index_01 != -1 && Index_intrare_01 != -1)
+            {
+                cout<<"Primul nod a fost identificat: Index "<<Index_01<<", Intrare "<<Index_intrare_01<<endl;
+                break;
+            }
+            else
+            {
+                cout<<"Nu a fost gasit un nod valid pentru primul click"<< endl;
+            }
+        }
+    }
+
+    while (running_desenare_legaturi)
+    {
+        int previous_x=-1, previous_y=-1;
+        if (ismouseclick(WM_LBUTTONDOWN))
+        {
+
+            int x = mousex();
+            int y = mousey();
+            clearmouseclick(WM_LBUTTONDOWN);
+
+            cout<<"Coordonatele pentru al doilea click: ("<<x<<", "<<y<<")"<<endl;
+
+            identificare_nod(running_desenare_legaturi, Index_02, Index_intrare_02, x, y);
+
+            if (Index_02 != -1 && Index_intrare_02 != -1)
+            {
+                cout<<"Nodul al doilea a fost identificat la: Index "<<Index_02<< ", Intrare "<<Index_intrare_02<<endl;
+
+                drawLine(piese[Index_01].intrari[Index_intrare_01].x, piese[Index_01].intrari[Index_intrare_01].y,piese[Index_02].intrari[Index_intrare_02].x,piese[Index_02].intrari[Index_intrare_02].y,RED);
+                graf[Index_01][Index_02].intrari[Index_intrare_01][Index_intrare_02]=1;
+                break;
+            }
+            else
+            {
+                cout<<"Niciun nod valid nu a fost gasit pentru al doilea click"<<endl;
+            }
+        }
+        /*else
+        {
+            int x=mousex();
+            int y=mousey();
+            drawLine(piese[Index_01].intrari[Index_intrare_01].x, piese[Index_01].intrari[Index_intrare_01].y, x, y, RED);
+            if(previous_x!=-1 && previous_y!=-1)
+            {
+                drawLine(piese[Index_01].intrari[Index_intrare_01].x, piese[Index_01].intrari[Index_intrare_01].y, previous_x, previous_y, BLACK);
+            }
+            previous_x=x; previous_y=y;
+        }*/
+    }
+}
+
+void Tool_Cases(int index)
+{
+    switch (index)
+    {
+        case 0:
+        {
+            desenare_intrari();
+            trasare_legatura();
+            //redraw();
+            break;
+        }
+        case 1:
+        {
+            int state=0;
+            while (!ismouseclick(WM_LBUTTONDOWN))
+            {
+                AnimareChenar(state);
+                desenare_intrari();
+                delay(500);
+                state=1-state;
+                cleardevice();
+                redraw();
+            }
+            break;
+        }
+        default:
+            return;
+    }
+}
+
+int main()
+{
     citire_figuri();
     initwindow(LATIME_ECRAN, INALTIME_ECRAN, "Electron");
     ///drawing the Toolbar and ItemMenu
@@ -273,6 +525,7 @@ int main() {
     DeseneazaBaraDeTools();
 
     /// Initializing the main() variables
+    int Tool_Selectat = -1;
     int Item_Selectat = -1; // no item has yet been selected
     bool running = true;
 
@@ -286,29 +539,13 @@ int main() {
 
             if (x<=LATIME_TOOLBAR && y>INALTIMEA_BAREI_DE_ITEME)
             {
-                int toolSelectat=(y-INALTIMEA_BAREI_DE_ITEME)/((INALTIME_ECRAN-INALTIMEA_BAREI_DE_ITEME)/NR_TOOLS);
-                int state=0;
-                while (!ismouseclick(WM_LBUTTONDOWN))
-                {
-                    animareChenar(state);
-                    delay(500);
-                    state=1-state;
-                    cleardevice();
-                    redraw();
-                }
-                if (toolSelectat==3) //stergere
-                {
-                    x=mousex();
-                    y=mousey();
-                    piesa pseudopiesa;
-                    pseudopiesa.x1=pseudopiesa.x2=x;
-                    pseudopiesa.y1=pseudopiesa.y2=y;
-                    for (int i=0; i<=nrPiese;++i)
-                        if (seIntersecteaza(piese[i],pseudopiesa))
-                            stergePiesa(i);
-                    cleardevice();
-                    redraw();
-                }
+                Tool_Selectat=getToolIndex(x, y);
+                cout << "Numarul Uneltei selectate " << Tool_Selectat << endl;
+            }
+            else if(Tool_Selectat !=-1)
+            {
+                Tool_Cases(Tool_Selectat);
+                Tool_Selectat=-1;
             }
             if (y <= INALTIMEA_BAREI_DE_ITEME)
             {
@@ -329,7 +566,6 @@ int main() {
                 Item_Selectat=-1;
             }
         }
-
 
     }
 
